a7426e40c3a1ad2c715d453f0775a77632fc1375
subrat <subrat.sahu@skill-mine.com>
2024-07-29 16:45:51 +0530

    Skillmine Auth SDK integration example

---
+++ app/src/main/java/com/skillmine/skillmineauthsdkexample/AuthenticationConstants.kt
@@ -0,0 +1,7 @@
+package com.skillmine.skillmineauthsdk_example
+
+object AuthenticationConstants {
+    const val BASE_URL = "base_url"
+    const val CLIENT_ID = "client_id"
+    const val REDIRECT_URL = "redirect_url"
+}
---
+++ app/src/main/java/com/skillmine/skillmineauthsdkexample/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.skillmine.skillmineauthsdkexample
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.skillmine.skillmineauthsdk_example.AuthenticationConstants.BASE_URL
+import com.skillmine.skillmineauthsdk_example.AuthenticationConstants.CLIENT_ID
+import com.skillmine.skillmineauthsdk_example.AuthenticationConstants.REDIRECT_URL
+import com.skillmine.webauthsdk.AuthenticationActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var loginButton: Button
+
+    //Get the result from AuthenticationActivity
+    private val authActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val accessToken = result.data?.getStringExtra("access_token")
+                accessToken?.let {
+                    //Open Dashboard Activity
+                    startActivity(Intent(this@MainActivity, DashBoardActivity::class.java))
+                    Log.v("ACCESS_TOKEN", accessToken)
+                }
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        loginButton = findViewById<Button>(R.id.bt_login)
+
+        //Call WebAuth Library
+        loginButton.setOnClickListener {
+            val intent =
+                AuthenticationActivity.createIntent(this, BASE_URL, CLIENT_ID, REDIRECT_URL)
+            authActivityResultLauncher.launch(intent)
+
+        }
+    }
+}
